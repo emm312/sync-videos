29e7d66362d882e8a5e622fb8ee9d4ac94757c14
emm312 <emmtbm55@gmail.com>
2022-11-04 21:33:36 +1100

    styles finally committed

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var player;
 
 function startplayer() 
 {
- player = document.getElementById('video_player');
+ player = document.getElementById('video-player');
  player.controls = false;
 }
 function play_vid()
@@ -31,27 +31,32 @@ function fullscreen() {
 }
 
 function sync() {
-    let start = 1651987825000;
+    let start = 59999999999999999;
     // change the timestart element based on how long until it starts
     
     if (new Date().getTime()>= start) {
+        
         // shpw the wrapper
-        document.getElementById("wrapper").style.visibility = "visible";
+        document.getElementById("player").style.display = "block";
+
         play_vid();
         let curr = new Date();
         // round curr to nearest second
-        let time = (curr - start);
+        let time = (curr.getTime() - start);
         // round time to 2 decimal place
         time /= 1000;
         var seconds = Math.round(time);
-
+        console.log(seconds);
         // if the playback is more than 2 seconds out of sync, sync it
         if (Math.abs(player.currentTime - seconds) > 2) {
             player.currentTime = seconds;
         }
         
-
         document.getElementById("waiting_for_start").style.display = "none";
+        document.getElementById("welkom").innerHTML = "Now playing a video";
+        document.body.style.fontSize = "10px";
+        document.getElementById("main").style.textAlign = "center";
+        document.getElementById("footer").style.display="none"
         
     } else {
         let timeStart = document.getElementById("waiting_for_start");
@@ -59,9 +64,9 @@ function sync() {
         let time = start - curtime.getTime();
         // convert time to minutes and seconds and hours
         let hours = Math.floor(time / (1000 * 60 * 60));
-        let minutes = Math.floor(time / 60000);
-        let seconds = Math.floor((time % 60000) / 1000);
-        timeStart.innerHTML = hours + " hours "+ minutes + " minutes and " + seconds + " seconds until the movie starts!";
+        let minutes = Math.floor(time / (1000 * 60)) % 60;
+        let seconds = Math.floor(time / 1000) % 60;
+        timeStart.innerHTML = hours + " hours "+ minutes + " minutes and " + seconds + " seconds until the video starts!";
     }
 }
 
